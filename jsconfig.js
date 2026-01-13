353fc4a8adf84c4552ae98ff1755f59240172cbf
Leonardo <32466022+leonardo141001@users.noreply.github.com>
2026-01-13 20:00:40 -0300

    Add files via upload

---
+++ jsconfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCOyhVCdV1dUWdgQXnPWhRHTb6xVYn68bk",
+  authDomain: "spq-v1.firebaseapp.com",
+  projectId: "spq-v1",
+  storageBucket: "spq-v1.firebasestorage.app",
+  messagingSenderId: "472363662852",
+  appId: "1:472363662852:web:10517568e7a010ff203f51"
+};
